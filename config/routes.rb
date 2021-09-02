cbea8119921d2d0d35da9d98116e6bdce776ef9c
hiroshi <hiroshi3110@gmail.com>
2021-09-02 21:26:36 +0900

    Breadcrumbs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: redirect('/cards')
   get    '/cards' => 'cards#index'
   post   '/cards' => 'cards#create'
-  get    '/cards/new' => 'cards#new'#, as: :new_card
+  # get    '/cards/new' => 'cards#new'#, as: :new_card
   get    '/card/:id' => 'cards#show', as: :card
   delete '/card/:id' => 'cards#destroy'
   # patch  '/card/:'
